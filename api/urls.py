908a40eeb54a26dc8e6c764e26bb04e3d0daa3dd
Amandeep Panesar <amandeep.s.panesar@gmail.com>
2019-09-04 14:01:37 -0400

    Populate DB with example data

--- api/urls.py
+++ api/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^', include('carfinds.urls'))
 ]
