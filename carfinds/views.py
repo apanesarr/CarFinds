b5b3d1e550293c7a3ccfef978266ca9bd1c6fea8
Amandeep Panesar <amandeep.s.panesar@gmail.com>
2019-09-17 11:32:51 -0400

    Paginate results

--- carfinds/views.py
+++ carfinds/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
 from .models import Car
@@ -9,10 +10,8 @@ from .permissions import IsAuthenticated
 class get_cars(ListCreateAPIView):
     permission_class = IsAuthenticated
     serializer_class = CarSerializer
+    pagination_class = PageNumberPagination
     search_fields = ['name', 'vendor']
     filter_backends = (filters.SearchFilter,)
     queryset = Car.objects.all()
-    #serialzer = CarSerializer(queryset, many=True)
-    #def get(self, request):
-  
-    #   return Response(serialzer.data, status=status.HTTP_200_OK)
+
